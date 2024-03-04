b9d61b529a63ccb411187563852b71e0bb3577b0
dragor.dev <dragor.dev@gmail.com>
2024-03-04 14:41:39 +0100

    Finished part-31/32-control-the-user-dynamic-menu.

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -18,12 +18,9 @@
                     <!--~~~ MENU LIST ~~~~~~-->
                     <div class="_1side_menu_list">
                         <ul class="_1side_menu_list_ul">
-                            <li><router-link to="/"><Icon type="ios-speedometer" /> Dashboard</router-link></li>
-                            <li><router-link to="/tags"><Icon type="ios-speedometer" /> Tags</router-link></li>
-                            <li><router-link to="/category"><Icon type="ios-speedometer" /> Category</router-link></li>
-                            <li><router-link to="/adminusers"><Icon type="ios-speedometer" /> Admin users</router-link></li>
-                            <li><router-link to="/role"><Icon type="ios-speedometer" /> Role Management</router-link></li>
-                            <li><router-link to="/assign-role"><Icon type="ios-speedometer" /> Assign Role</router-link></li>
+                            <li v-for="(menuItem, i) in permission" :key="i" v-if="permission.length && menuItem.read">
+                                <router-link :to="menuItem.name"><Icon type="ios-speedometer" /> {{menuItem.resourceName}}</router-link>
+                            </li>
                             <li><a href="/logout"><Icon type="ios-speedometer" /> LogOut</a></li>
 
                         </ul>
@@ -62,7 +59,7 @@
 <script>
     import store from "./store";
     export default {
-        props: ['user'],
+        props: ['user', 'permission'],
         data() {
             return {
                 isLoggedIn: false,
